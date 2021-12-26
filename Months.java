03afbd8a302ec16b633e6316ced66439181bb2df
Ashtalakshmi8 <ASHTALAKSHMI8>
2021-12-26 20:16:19 +0530

    if-else-if-conditional state

---
+++ Months.java
@@ -0,0 +1,44 @@
+class Months{
+      public static void main(String [] args ){
+int day=13;
+if( day==1){
+System.out.println( "january ");
+}
+else if (day==2){
+System.out.println( "February ");
+}
+else if (day==3){
+System.out.println( "March ");
+}
+else if (day==4){
+System.out.println( "April ");
+}
+else if (day==5){
+System.out.println( "May ");
+}
+else if (day==6){
+System.out.println( "june ");
+}
+else if (day==7){
+System.out.println( "July ");
+}
+else if (day==8){
+System.out.println( "August ");
+}
+else if (day==9){
+System.out.println( "September ");
+}
+else if (day==10){
+System.out.println( "October ");
+}
+else if (day==11){
+System.out.println( "November ");
+}
+else if (day==12){
+System.out.println( "December");
+}
+else if (day==13){
+System.out.println( "End");
+}
+}
+}
